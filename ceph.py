4f51816dcd674829f95b4d066d5334e44a08d286
astoycos <astoycos@redhat.com>
2020-05-20 12:26:00 -0400

    Finish Basic s3 API ceph example and add route/ ceph toolbox yamls

--- ceph.py
+++ ceph.py
@@ -1,26 +1,23 @@
 import boto3
 import wget
-import botocore
-import boto.s3.connection
-import os 
-import glob 
+import os  
+import base64
 
-
-s3_endpoint_url = ''
-s3_access_key_id = ""
-s3_secret_access_key = ""
-s3_bucket = 'MyBucket'
+s3_endpoint_url = "http://ceph-route-rook-ceph.apps.astoycos-ocp.shiftstack.com"
+s3_access_key_id = "NkZXSzY5WDI1MlMyRkU2Szk2RzU="
+s3_secret_access_key = "aEVJTjV4aERvRGQzMzh5WjVLQlYzNXk4QnVoWU9Ba3A5aThMMzlVSg=="
+s3_bucket = 'mybucket'
 
 # configure boto S3 client
 # Low level service access
 s3 = boto3.client('s3',
-                  '',
-                  use_ssl = False,
-                  verify = False,
-                  endpoint_url = s3_endpoint_url,
-                  aws_access_key_id = s3_access_key_id,
-                  aws_secret_access_key = s3_secret_access_key,
-                  )
+    '',
+    use_ssl = False,
+    verify = False,
+    endpoint_url = s3_endpoint_url,
+    aws_access_key_id = base64.decodebytes(bytes(s3_access_key_id,'utf-8')).decode('utf-8'),
+    aws_secret_access_key = base64.decodebytes(bytes(s3_secret_access_key, 'utf-8')).decode('utf-8'),
+    )
 
 # Configure an S3 Resource 
 # Higher level object oriented API 
@@ -29,34 +26,35 @@ s3r = boto3.resource('s3',
     use_ssl = False,
     verify = False,
     endpoint_url = s3_endpoint_url,
-    aws_access_key_id = s3_access_key_id,
-    aws_secret_access_key = s3_secret_access_key,)
-
-#Configure and S3 Connection 
-conn = boto.connect_s3(
-        aws_access_key_id = s3_access_key_id,
-        aws_secret_access_key = s3_secret_access_key,
-        host = s3_endpoint_url,
-        )
+    aws_access_key_id = base64.decodebytes(bytes(s3_access_key_id,'utf-8')).decode('utf-8'),
+    aws_secret_access_key = base64.decodebytes(bytes(s3_secret_access_key, 'utf-8')).decode('utf-8'),
+)
 
 response = s3.list_buckets()
-
 # Get a list of all bucket names from the response
 buckets = [bucket['Name'] for bucket in response['Buckets']]
 
 # Print out the bucket list
-print("Bucket List: %s" % buckets)
-print("Trying to make 'MyBucket'")
-try:
-    s3.create_bucket(Bucket=s3_bucket)
-except: 
-    print("Bucket " + s3_bucket + " already exists")
+print("Initial bucket List: %s" % buckets)
 
+#s3r.Bucket("MyBucket").objects.all().delete()
+#s3.delete_bucket(Bucket="MyBucket")
+
+print("Trying to make 'mybucket'")
+if s3_bucket not in buckets:
+    s3.create_bucket(Bucket=s3_bucket)
+else: 
+    print("Bucket " + s3_bucket + " already exists, deleting and recreating")
+    s3r.Bucket(s3_bucket).objects.all().delete()
+    s3.delete_bucket(Bucket=s3_bucket)
+    s3.create_bucket(Bucket=s3_bucket)
+
+response = s3.list_buckets()
 #Get all buckets 
 buckets = [bucket['Name'] for bucket in response['Buckets']]
 
 # Print out the bucket list
-print("Bucket List: %s" % buckets)
+print("Updated bucket List: %s" % buckets)
 
 print("Upload some demo images to Ceph Object Storage")
 
@@ -66,31 +64,36 @@ s3.upload_file("demo-files/pic-2.jpeg", s3_bucket, "demo-pic-2.jpeg")
 s3.upload_file("demo-files/pic-3.jpeg", s3_bucket, "demo-pic-3.jpeg")
 s3.upload_file("demo-files/pic-4.jpeg", s3_bucket, "demo-pic-4.jpeg")
 
-print("List information about files currently stored in ceph")
+print("List files currently stored in ceph")
 
-bucket = conn.get_bucket(s3_bucket)
+my_bucket = s3r.Bucket(name=s3_bucket)
 
-for key in bucket.list():
-        print(key.name+ " "+key.sizekey + " "+ key.last_modified)
+for my_bucket_object in my_bucket.objects.all():
+    print(my_bucket_object.key + " was last modified at " + str(my_bucket_object.last_modified))
 
+    
 print("Download files from ceph")
-s3.download_file(s3_bucket,"demo-pic-1.jpeg","demo-files-out/pic-1.jpeg")
-s3.download_file(s3_bucket,"demo-pic-2.jpeg","demo-files-out/pic-2.jpeg")
-s3.download_file(s3_bucket,"demo-pic-3.jpeg","demo-files-out/pic-3.jpeg")
-s3.download_file(s3_bucket,"demo-pic-4.jpeg","demo-files-out/pic-4.jpeg")
+s3.download_file(s3_bucket,"demo-pic-1.jpeg","demo-files-out/pic-1-out.jpeg")
+s3.download_file(s3_bucket,"demo-pic-2.jpeg","demo-files-out/pic-2-out.jpeg")
+s3.download_file(s3_bucket,"demo-pic-3.jpeg","demo-files-out/pic-3-out.jpeg")
+s3.download_file(s3_bucket,"demo-pic-4.jpeg","demo-files-out/pic-4-out.jpeg")
 
 ## clean up 
 print("Remove bucket from ceph")
 
-s3.delete_bucket(Bucket="MyBucket")
+my_bucket.objects.all().delete()
+s3.delete_bucket(Bucket=s3_bucket)
+
+print("Downloaded images from ceph can be seen in 'demo-files-out/' directory")
 
 print("Delete downloaded files in `/demo-files-out`? y/n")
 delete= input()
 
 if delete == "y":
-    files = glob.glob('/demo-files-out')
-    for f in files:
-        os.remove(f)
+    mydir = "demo-files-out/"
+    filelist = [ f for f in os.listdir(mydir)]
+    for f in filelist:
+        os.remove(os.path.join(mydir, f))
 else: 
    print("Downloaded files remain in '/demo-files-out' folder")
 
